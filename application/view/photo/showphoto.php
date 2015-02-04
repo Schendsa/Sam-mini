57ade08c2967f7fb1108dbcd9585e18efa3130dc
Schendsa <sam.van.schendel@hotmail.nl>
2015-02-04 23:37:50 +0100

    Revert "First upload"
    This reverts commit 723996f08279fcb6d2eae20498deab07d7a3d334.

--- application/view/photo/showphoto.php
+++
@@ -1,7 +0,0 @@
-<div class="container">
-	<h1><?php echo $this->photo->title ?></h1>
-	<div class="box">
-		<img src="<?php echo URL . 'public/img/' . $this->photo->title . '.' . $this->photo->fileType ?>"><br>
-		<a href="<?php echo URL . 'game/play/' . $this->photo->id ?>">Play game with this photo</a>
-    </div>
-</div>
